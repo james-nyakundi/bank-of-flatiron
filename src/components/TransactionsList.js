7ad18b7d01212541e47e92d9bf1ce69fc48b2a30
james nyakundi <omarejames082@gmail.com>
2024-05-01 11:12:07 +0300

    done

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,22 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Transaction from "./Transaction";
 
-const handleSubmit=()=>{}
-fetch('http://localhost:8001/transactions', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    date: '2022-12-31',
-    description: 'Sample Description',
-    category: 'Sample Category',
-    amount: 100.50,
-  }),
-})
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
 
 function TransactionsList() {
   const [transactions,setTransctions] = useState([])
@@ -29,64 +13,45 @@ function TransactionsList() {
   });
 },[])
 console.log(transactions)
-function Search({ transactions, setFilteredTransactions }) {
-  const [searchTerm, setSearchTerm] = useState('');
-
-  
-  const handleSearchChange = (event) => {
-    const searchTerm = event.target.value.toLowerCase();
-    setSearchTerm(searchTerm);
-
-    
-    const filteredTransactions = transactions.filter(transaction =>
-      transaction.description.toLowerCase().includes(searchTerm)
-    );
-
-    
-    setFilteredTransactions(filteredTransactions);
-  }};
-  
-
-
-  return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
+return (
+  <table className="ui celled striped padded table">
+    <tbody>
+      <tr>
+        <th>
+          <h3 className="ui center aligned header">Date</h3>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Description</h3>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Category</h3>
+        </th>
+        <th>
+          <h3 className="ui center aligned header">Amount</h3>
+        </th>
+      </tr>
+      {/* render a list of <Transaction> components here */}{
+        transactions && transactions.map((trans)=>(
+          
+          <tr
+            key={trans.id}>
           <th>
-            <h3 className="ui center aligned header">Date</h3>
+            <p> {trans.date}</p>
           </th>
           <th>
-            <h3 className="ui center aligned header">Description</h3>
+            <p> {trans.description}</p>
           </th>
           <th>
-            <h3 className="ui center aligned header">Category</h3>
+            <p> {trans.category}</p>
           </th>
           <th>
-            <h3 className="ui center aligned header">Amount</h3>
+            <p>{trans.amount}</p>
           </th>
         </tr>
-        {/* render a list of <Transaction> components here */}{
-          transactions && transactions.map((trans)=>(
-            
-            <tr>
-            <th>
-              <p> {trans.date}</p>
-            </th>
-            <th>
-              <p> {trans.description}</p>
-            </th>
-            <th>
-              <p> {trans.category}</p>
-            </th>
-            <th>
-              <p>{trans.amount}</p>
-            </th>
-          </tr>
-          ))
-        }
-      </tbody>
-    </table>
-  );
+        ))
+      }
+    </tbody>
+  </table>
+);
 }
-
 export default TransactionsList;
