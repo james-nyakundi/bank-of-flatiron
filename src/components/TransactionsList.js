0954cc6f9a465cd6e65fa70fb112828caa54ef9f
james nyakundi <omarejames082@gmail.com>
2024-05-03 13:24:56 +0300

    done

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,57 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Transaction from "./Transaction";
-
-
-function TransactionsList() {
-  const [transactions,setTransctions] = useState([])
-  useEffect(()=>{
-    
-  fetch("http://localhost:8001/transactions")
-  .then(response => response.json())
-  .then(data => {
-    setTransctions(data)
-  });
-},[])
-console.log(transactions)
-return (
-  <table className="ui celled striped padded table">
-    <tbody>
-      <tr>
-        <th>
-          <h3 className="ui center aligned header">Date</h3>
-        </th>
-        <th>
-          <h3 className="ui center aligned header">Description</h3>
-        </th>
-        <th>
-          <h3 className="ui center aligned header">Category</h3>
-        </th>
-        <th>
-          <h3 className="ui center aligned header">Amount</h3>
-        </th>
-      </tr>
-      {/* render a list of <Transaction> components here */}{
-        transactions && transactions.map((trans)=>(
-          
-          <tr
-            key={trans.id}>
+function TransactionsList({transactions}) {
+  const list = transactions.map((item)=>{
+    return <Transaction key={item.id} date={item.date} description={item.description} category={item.category} amount={item.amount} />;
+  })
+  return (
+    <table className="ui celled striped padded table">
+      <tbody>
+        <tr>
           <th>
-            <p> {trans.date}</p>
+            <h3 className="ui center aligned header">Date</h3>
           </th>
           <th>
-            <p> {trans.description}</p>
+            <h3 className="ui center aligned header">Description</h3>
           </th>
           <th>
-            <p> {trans.category}</p>
+            <h3 className="ui center aligned header">Category</h3>
           </th>
           <th>
-            <p>{trans.amount}</p>
+            <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        ))
-      }
-    </tbody>
-  </table>
-);
+        {/* render a list of <Transaction> components here */}
+        {list}
+      </tbody>
+    </table>
+  );
 }
+
 export default TransactionsList;
