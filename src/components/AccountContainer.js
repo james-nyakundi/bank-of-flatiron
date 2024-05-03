713caa46c167a1c693f67d82087292c739178a44
james nyakundi <omarejames082@gmail.com>
2024-05-03 13:01:57 +0300

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -6,7 +6,11 @@ import AddTransactionForm from "./AddTransactionForm";
 function AccountContainer() {
   const [transaction, setTransaction] = useState([])
   const [query, setQuery] = useState("")
-  
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions?q=" + query)
+      .then((resp) => resp.json())
+      .then(transaction => setTransaction(transaction))
+  }, [query])
   function handleSearch(e) {
     setQuery(e.target.value)
   }
